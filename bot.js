c0bd61b0e14b0f4c285d538351e85dddc77d2d80
weretybe <56060270+weretybe@users.noreply.github.com>
2020-07-15 20:32:19 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ client.on('message', msg => {
   if (msg.content === '!commands') {
     msg.channel.send('Right now I know airhorn, bot, and maps, and quotelist.');  }     
   if (msg.content === '!quotelist') {
-    msg.channel.send('Currently, here are the prefixes for the ![prefix]quote command and the number of quotes for each: ty(6 - pants, rarity, furries, otter, spider, king), chris(3 - whales, bones, hawks), npc(5), scott(3 - macgyver, dragon, cuck), kristyn(5 - puppet, king, hedgehog, santa, bear), eth(1), austin(5 - possum, volcanoes, magic, funeral, coke), thunder(2 - blind, paintball), devan(5 - pony, gas, tasteful, genocide, neko), jake(3 - cheese, believe, baby), megan(1), beth(1). '); }    
+    msg.channel.send('Currently, here are the prefixes for the ![prefix]quote command and the number of quptes for each: ty(5), Chris(2), npc(5), scott(3), kristyn(5), eth(1), austin(5), thunder(2), devan(5), jake(3), megan(1), beth(1). '); }    
   if (msg.content === '!maps') {
     msg.channel.send('I have guided maps for !houston and !nola'); }  
   if (msg.content === '!houston') {
@@ -22,7 +22,7 @@ client.on('message', msg => {
   if (msg.content === '!nola') {
     msg.channel.send('https://drive.google.com/open?id=174k_24ILN6YkEWdVbTO4x6uGDW2HFRw8'); } 
   
-  //Quote Calling Messages       
+    //Quotes        
   if (msg.content === '!tyquote') {
     msg.channel.send(tyquote()); }       
   if (msg.content === '!chrisquote') {
@@ -47,90 +47,15 @@ client.on('message', msg => {
     msg.channel.send("'Boobies, and you can quote me on that' - Megan Vasquez, 2019"); }
    if (msg.content === '!bethquote') {
     msg.channel.send("'If you cum even a single bee inside a girl, you are never going to fuck her again' - Beth DeLeon, 2018"); } 
-	
-  //Individual Quotes
-  if (msg.content === '!tyquote pants') {
-    msg.channel.send("'You can't naked dat butt with **pants** on.' - Ty Oden, 2017"); }  
-  if (msg.content === '!tyquote rarity') {
-    msg.channel.send("'If we talking about best fashonista and you are talking some other filly's name besides **Rarity** you need to spit that trash out of your dick catcher you ignorant fuckass.' - Ty Oden, 2017"); }  
-  if (msg.content === '!tyquote furries') {
-    msg.channel.send("'**Furries** are the blackface of dogs' - Ty Oden, 2018"); }  
-  if (msg.content === '!tyquote otter') {
-    msg.channel.send("'This **otter** is stupid thicc and the Monteray Bay Aquarium should not have to apoligize for fat shaming their own otter.' - Ty Oden, 2019"); }  
-  if (msg.content === '!tyquote spider') {
-    msg.channel.send("'That's how you get eaten by a big **spider** disguised as a sexy person' - Ty Oden, 2020"); }  
-  if (msg.content === '!tyquote king') {
-    msg.channel.send("'Don't let any 5-year age gap or bounce house dominatrix get you down. You're a **King**.' - Ty Oden, 2020"); }  	
-
-  if (msg.content === '!chrisquote whales') {
-    msg.channel.send("'I think we need to purge the **whales**' - Chris Johnson, 2017"); } 
-  if (msg.content === '!chrisquote hawks') {
-    msg.channel.send("'You have the **hawks** of an eagle.' - Chris Johnson, 2014"); } 
-  if (msg.content === '!chrisquote bones') {
-    msg.channel.send("*After vomiting 'Oh that drink.. Oof my **bones**' - Chris Johnson, aproximately 8 seconds into 2019."); } 	
-
-  if (msg.content === '!scottquote macgyver') {
-    msg.channel.send("'I'm going to **MacGyver** my dick into my own ass!' -Scott Gilfoil, 2017"); } 	
-  if (msg.content === '!scottquote dragon') {
-    msg.channel.send("'[That] is literally my only utility in a world where Devan is a **dragon**' - Scott Gilfoil on Dragon Fucking, 2018"); } 	
-  if (msg.content === '!scottquote cuck') {
-    msg.channel.send("'Is it possible to **cuck** yourself?' - Scoott Gilfoot, 2018"); } 		
-
-  if (msg.content === '!kristynquote puppet') {
-    msg.channel.send("'It's basically like ass-to-mouthing a **puppet**, but yelling while you do it so that your words come out of its mouth' - Kristyn Fusco 2017"); } 
-  if (msg.content === '!kristynquote king') {
-    msg.channel.send("''Financially assault me, **King**-Daddy' - Ty Oden' - Kristyn Fusco, 2019"); } 
-  if (msg.content === '!kristynquote santa') {
-    msg.channel.send("'I mean, fuck. If I could hoodwink a **Santa** into sugar-daddying me a DIAMOND MINE, I'd be jingling those bells all the way to the BANK!' - Kristyn Fusco, noted Santa Fetishist, 2018"); } 
-  if (msg.content === '!kristynquote hedgehog') {
-    msg.channel.send("'Why can't an anthropomorphic **hedgehog** pull a Marilyn Manson in the privacy of in front of his own personal webcam?' - Kristyn Fusco, 2017"); } 
-  if (msg.content === '!kristynquote bear') {
-    msg.channel.send("'I want **bear** meet/Say hello to bear/Meet and greet/Meet and... yeet' - Kristyn Fusco, 2018"); } 
-  if (msg.content === '!kristynquote *') {
-    msg.channel.send(*); } 
-
-  if (msg.content === '!austinquote possum') {
-    msg.channel.send("'**Possum** blood is high in alcohol content. Trust me; I'm a scientist' - Austin McDaniel, 2017"); } 	
-  if (msg.content === '!austinquote magic') {
-    msg.channel.send("'I will mansplain the shit outta **magic** to you' - Austin McDaniel, 2018"); } 	
-  if (msg.content === '!austinquote funeral') {
-    msg.channel.send("'I also want a compilation of every pinned quote I've ever earned read out by whomever is presiding over my **funeral**.' - Austin McDaniel, 2019"); } 	
-  if (msg.content === '!austinquote volcanoes') {
-    msg.channel.send("'I don't think **volcanoes**... cum' - Austin McDaniel, 2019"); } 	
-  if (msg.content === '!austinquote coke') {
-    msg.channel.send("'Only thing worse than a coke dealer, is a **coke** dealer who refuses to go green' - Austin McDaniel, 2020"); } 	
-
-  if (msg.content === '!thunderquote blind') {
-    msg.channel.send("'If a level 1 **blind** man showed up at our door I would kick his ass up and down that street' - Thunder O'Brien 2017"); }  
-  if (msg.content === '!thunderquote paintball') {
-    msg.channel.send("'If we are ever on Queer Eye playing **paintball** I'm going to shoot the goddamn cameraman' - Thunder O'Brien 2018"); }  
-
-  if (msg.content === '!devanquote pony') {
-    msg.channel.send("'Now hang on just a darn toot'in minute. How the floppy fuck can someone be anti-**pony**?' - Devan Horn, 2017"); }  
-  if (msg.content === '!devanquote gas') {
-    msg.channel.send("'[Jake], did you dick the **gas** outta my car?!?' - Devan Horn, 2020"); }  
-  if (msg.content === '!devanquote tasteful') {
-    msg.channel.send("'There's one or two rapes, but they're **tasteful**' - Devan Horn, 2018"); }  
-  if (msg.content === '!devanquote *') {
-    msg.channel.send("'In all honesty it was a really fair and rational **genocide**' - Devan Horn, 2018"); }  
-  if (msg.content === '!devanquote *') {
-    msg.channel.send("'All **neko** genitalia are valid!' - Devan Horn, 2020"); }  
-
-  if (msg.content === '!jakequote cheese') {
-    msg.channel.send("'If your vag is stuffed with **cheese** no man mayo is getting in' - Jake, 2020"); }  
-  if (msg.content === '!jakequote believe') {
-    msg.channel.send("'I **believe** in your testicles, Troy' - Jake, 2019"); }  
-  if (msg.content === '!jakequote baby') {
-    msg.channel.send("'Oooo **baby**. I just wanna lay ya down a platonically watch a movie together so bad.' - Jake, 2020"); }  
 });
 
 //Quote Funtions
 function tyquote() {
-  var values = ["'You can't naked dat butt with **pants** on.' - Ty Oden, 2017","'If we talking about best fashonista and you are talking some other filly's name besides Rarity you need to spit that trash out of your dick catcher you ignorant fuckass.' - Ty Oden, 2017","'**Furries** are the blackface of dogs' - Ty Oden, 2018","'This **otter** is stupid thicc and the Monteray Bay Aquarium should not have to apoligize for fat shaming their own otter.' - Ty Oden, 2019","'That's how you get eaten by a big **spider** disguised as a sexy person' - Ty Oden, 2020","'Don't let any 5-year age gap or bounce house dominatrix get you down. You're a **King**.' - Ty Oden, 2020"],
+  var values = ["'You can't naked dat butt with pants on.' - Ty Oden, 2017","'If we talking about best fashonista and you are talking some other fillies name besides Rarity you need to spit that trash out of your dick catcher you ignorant fuckass.' - Ty Oden, 2017","'Furries are the blackface of dogs' - Ty Oden, 2018","'This otter is stupid thicc and the Monteray Bay Aquarium should not have to apoligize for fat shaming their own otter.' - Ty Oden, 2019","'That's how you get eaten by a big spider disguised as a sexy person' - Ty Oden, 2020"],
       valueToUse = values[Math.floor(Math.random() * values.length)]
   return(valueToUse) }
-function chrisjquote() {
-    var values = ["'I think we need to purge the **whales**' - Chris Johnson, 2017","'You have the **hawks** of an eagle.' - Chris Johnson, 2014","*After vomiting 'Oh that drink.. Oof my **bones**' - Chris Johnson, aproximately 8 seconds into 2019."],
+  function chrisjquote() {
+    var values = ["'I think we need to purge the whale[s]' - Chris Johnson, 2017", "'You have the hawks of an eagle.' - Chris Johnson, 2014","*After vomiting 'Oh that drink.. Oh my bones' - Chris Johnson, aproximately 8 seconds into 2019." ],
         valueToUse = values[Math.floor(Math.random() * values.length)]
     return(valueToUse) }
 function npcquote() {
@@ -138,11 +63,11 @@ function npcquote() {
         valueToUse = values[Math.floor(Math.random() * values.length)]
     return(valueToUse) }
 function scottquote() {
-    var values = ["'I'm going to **MacGyver** my dick into my own ass!' -Scott Gilfoil, 2017","'[That] is literally my only utility in a world where Devan is a **dragon**' - Scott Gilfoil on Dragon Fucking, 2018","'Is it possible to **cuck** yourself?' - Scoott Gilfoot, 2018"]
+    var values = ["'I'm going to MacGyver my dick into my own ass!' -Scott Gilfoil, 2017","'this is literally my only utility in a world where Devan is a dragon' - Scott Gilfoil on Dragon Fucking, 2018","'Is it possible to cuck yourself?' - Scoott Gilfoot, 2018"]
         valueToUse = values[Math.floor(Math.random() * values.length)]
     return(valueToUse) }
 function kristynquote() {
-  var values = ["'It's basically like ass-to-mouthing a **puppet**, but yelling while you do it so that your words come out of its mouth' - Kristyn Fusco 2017","''Financially assault me, **King**-Daddy' - Ty Oden' - Kristyn Fusco, 2019","'I mean, fuck. If I could hoodwink a **Santa** into sugar-daddying me a DIAMOND MINE, I'd be jingling those bells all the way to the BANK!' - Kristyn Fusco, noted Santa Fetishist, 2018","'Why can't an anthropomorphic **hedgehog** pull a Marilyn Manson in the privacy of in front of his own personal webcam?' - Kristyn Fusco, 2017","'I want **bear** meet/Say hello to bear/Meet and greet/Meet and... yeet' - Kristyn Fusco, 2018"],
+  var values = ["'It's basically like ass-to-mouthing a puppet, but yelling while you do it so that your words come out of its mouth' - Kristyn Fusco 2017","''Financially assault me, King-Daddy' - Ty Oden' - Kristyn Fusco, 2019","'I mean, fuck. If I could hoodwink a Santa into sugar-daddying me a DIAMOND MINE, I'd be jingling those bells all the way to the BANK!' - Kristyn Fusco, noted Santa Fetishist, 2018","'Why can't an anthropomorphic hedgehog pull a Marilyn Manson in the privacy of in front of his own personal webcam?' - Kristyn Fusco, 2017","'I want bear meet/Say hello to bear/Meet and greet/Meet and... yeet' - Kristyn Fusco, 2018"],
         valueToUse = values[Math.floor(Math.random() * values.length)]
     return(valueToUse) }
 function ethquote() {
@@ -150,19 +75,19 @@ function ethquote() {
         valueToUse = values[Math.floor(Math.random() * values.length)]
     return(valueToUse) }  
 function austinquote() {
-    var values = ["'**Possum** blood is high in alcohol content. Trust me; I'm a scientist' - Austin McDaniel, 2017","'I will mansplain the shit outta **magic** to you' - Austin McDaniel, 2018","'I also want a compilation of every pinned quote I've ever earned read out by whomever is presiding over my **funeral**.' - Austin McDaniel, 2019","'I don't think **volcanoes**... cum' - Austin McDaniel, 2019","'Only thing worse than a coke dealer, is a **coke** dealer who refuses to go green' - Austin McDaniel, 2020"]
+    var values = ["'Possum blood is high in alcohol content. Trust me; I'm a scientist' - Austin McDaniel, 2017","'I will mansplain the shit outta magic to you' - Austin McDaniel, 2018","'I also want a compilation of every pinned quote I've ever earned read out by whomever is presiding over my funeral.' - Austin McDaniel, 2019","'I don't think volcanoes... cum' - Austin McDaniel, 2019","'Only thing worse than a coke dealer, is a coke dealer who refuses to go green' - Austin McDaniel, 2020"]
         valueToUse = values[Math.floor(Math.random() * values.length)]
     return(valueToUse) }  
 function thunderquote() {
-    var values = ["'If a level 1 **blind** man showed up at our door I would kick his ass up and down that street' - Thunder O'Brien 2017","'If we are ever on Queer Eye playing **paintball** I'm going to shoot the goddamn cameraman' - Thunder O'Brien 2018"]
+    var values = ["'If a level 1 blind man showed up at our door I would kick his ass up and down that street' - Thunder O'Brien 2017","'If we are ever on Queer Eye playing paintball I'm going to shoot the goddamn cameraman' - Thunder O'Brien 2018"]
         valueToUse = values[Math.floor(Math.random() * values.length)]
     return(valueToUse) }  
 function devanquote() {
-    var values = ["'Now hang on just a darn toot'in minute. How the floppy fuck can someone be anti-**pony**?' - Devan Horn, 2017","'[Jake], did you dick the **gas** outta my car?!?' - Devan Horn, 2020","'There's one or two rapes, but they're **tasteful**' - Devan Horn, 2018","'In all honesty it was a really fair and rational **genocide**' - Devan Horn, 2018","'All **neko** genitalia are valid!' - Devan Horn, 2020"]
+    var values = ["'Now hang on just a darn toot'in minute. How the floppy fuck can someone be anti-pony?' - Devan Horn, 2017","'[Jake], did you dick the gas outta my car?!?' - Devan Horn, 2020","'There's one or two rapes, but they're tasteful' - Devan Horn, 2018","'In all honesty it was a really fair and rational genocide' - Devan Horn, 2018","'All neko genitalia are valid!' - Devan Horn, 2020"]
         valueToUse = values[Math.floor(Math.random() * values.length)]
     return(valueToUse) }
 function jakequote() {
-    var values = ["'If your vag is stuffed with **cheese** no man mayo is getting in' - Jake, 2020","'I **believe** in your testicles, Troy' - Jake, 2019","'Oooo **baby**. I just wanna lay ya down a platonically watch a movie together so bad.' - Jake, 2020"]
+    var values = ["'If your vag is stuffed with cheese no man mayo is getting in' - Jake, 2020","'I believe in your testicles, Troy' - Jake, 2019","'Oooo baby. I just wanna lay ya down a platonically watch a movie together so bad.' - Jake, 2020"]
         valueToUse = values[Math.floor(Math.random() * values.length)]
     return(valueToUse) }    
 
