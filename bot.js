d62f2f303a5a61827ca5c44b5b4a1e44fe4be36b
weretybe <56060270+weretybe@users.noreply.github.com>
2021-09-18 10:43:22 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ client.on('message', msg => {
   if (msg.content === '!bot') {
     msg.channel.send('I am listening and taking notes'); }
   if (msg.content === '!version') {
-    msg.channel.send('I am Secretary Bot 2.16. I have 8 new Haiku. Seriously. We need to talk about how many quotes there are. Please help.'); }
+    msg.channel.send('I am Secretary Bot 2.17. If the function is working, I should be able to predict the future now. '); }
   if (msg.content === '!commands') {
     msg.channel.send('Right now I know airhorn, bot, haiku, and quotelist.');  }     
   if (msg.content === '!quotelist') {
@@ -21,6 +21,11 @@ client.on('message', msg => {
 //Haiku Function 
  if (msg.content === '!haiku') {
     msg.channel.send(haikupicture()); msg.channel.send(haikuwords()); }       
+
+//Tarot Function
+ if (msg.content === '!tarot1') {
+    msg.channel.send('The seer shuffles the deck. A smile. A wink. A deft hand motion, almost hidden beneath the Seer's guile.'); msg.channel.send('Is this your card?'); msg.channel.send(haikupicture()); }
+
 	
 //Quotes        
   if (msg.content === '!tyquote') {
